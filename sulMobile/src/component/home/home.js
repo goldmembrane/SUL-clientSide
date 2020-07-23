a331afefb9f21b7fea8351df087d01c36572129e
JunSeobKim <JunSeobKim@github.com>
2020-07-23 16:26:01 +0900

    Header navi bar CSS - Pending navigation frame and css

---
+++ sulMobile/src/component/home/home.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  ImageBackground,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+export default function HomeScreen() {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <ImageBackground
+        source={require('../../../assets/background/mainBackground1.jpg')}
+        style={styles.bgImage}>
+        <Text style={styles.text}>{`     소송을 
+     소송답게`}</Text>
+      </ImageBackground>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  bgImage: {
+    flex: 1,
+    justifyContent: 'center',
+    width: '100%',
+    height: '100%',
+  },
+  text: {
+    fontSize: 30,
+    color: '#ffffff',
+  },
+});
---
+++ sulMobile/src/component/information/information.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+export default function Information() {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>sul team!</Text>
+    </View>
+  );
+}
