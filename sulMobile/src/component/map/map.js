e39aed2e74a19213eaec39cd0a97877c2166425d
김준섭 <kimjunseob@gimjunseob-ui-MacBookPro.local>
2020-08-05 11:36:29 +0900

    Completed - Map page HTML CSS - and signin signup check validation

--- sulMobile/src/component/map/map.js
+++ sulMobile/src/component/map/map.js
@@ -17,10 +17,29 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+
+const styles = StyleSheet.create({
+  map: {
+    flex: 1,
+    height: '100%',
+    width: '100%',
+  },
+});
+
 export default function Map() {
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Map!</Text>
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.map}
+        initialRegion={{
+          latitude: 37.4959854,
+          longitude: 127.0664091,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
     </View>
   );
 }
