12e727bdf220dbd4c42e815a3594eb3ca2c2f218
조규창 <cho@jogyuchang-ui-MacBookPro.local>
2020-08-09 14:22:36 +0900

    ADD saga file

--- sulMobile/src/store/modules/sagas.js
+++ sulMobile/src/store/modules/sagas.js
@@ -1,17 +1,24 @@
-import {call, put, takeEvery, takeLatest} from 'redux-saga/effects';
+import {delay} from 'redux-saga';
+import {call, put, takeEvery, takeLatest, all} from 'redux-saga/effects';
 import {REQUEST_API_DATA} from './actions';
 // import {fetchData} from './api';
 import {fetchDismiss} from '../../component/helper/fetchDismissApi';
-
+import {fetchAcc} from '../../component/helper/fetchDismissApi';
+import {fetchHello} from './ex';
 import {putDisMiss} from './dismissModule';
 // worker Saga: will be fired on USER_FETCH_REQUESTED actions
 function* getApiData(action) {
   try {
     // do api call
     const data = yield call(fetchDismiss, action.text);
-    // console.log(data.data, 'yield!!!');
+    // const data = yield call(fetchHello);
+    console.log(data.data, 'yield!!!');
     // yield put(putDisMiss(data.data));
-    // yield put({type: 'DISSMISS', dismiss: data.data});
+    yield put({type: 'DISSMISS', dismiss: data.data});
+    const aData = yield call(fetchAcc, action.text);
+    yield put({type: 'ACC', acc: aData.data});
+    yield delay(8000);
+    yield put({type: 'ISLODING', isLoding: false});
   } catch (e) {
     console.log(e, 'saga failed');
   }
@@ -24,6 +31,6 @@ function* getApiData(action) {
   dispatched while a fetch is already pending, that pending fetch is cancelled
   and only the latest one will be run.
 */
-export default function* mySaga() {
+export default function* mySaga(action) {
   yield takeEvery(REQUEST_API_DATA, getApiData);
 }
